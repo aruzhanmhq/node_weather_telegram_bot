169286bc8a7a70e9ab37747840a6730bd32f3c9f
Your Name <you@example.com>
2022-06-07 14:05:23 +0600

    telegram bot

--- app.js
+++ app.js
@@ -1,45 +1,28 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios')
 
-const token = '';
+const token = '5349385005:AAGhDGHaX-aoRIq_R-TTFCH0vhQ1CxqfKFE';
 
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    let cityName = msg.text;
-    // if(msg.text.includes('иди')){
-    //     bot.sendMessage(chatId, 'Сам иди');
-    // }else{
-    //     bot.sendMessage(chatId, 'Привет');
-    // }
+  const chatId = msg.chat.id;
+  let cityName = msg.text;
 
-    // const OPEN_WEATHER_MAP_URL = 'https://community-open-weather-map.p.rapidapi.com/weather';
-    // const OPEN_WEATHER_MAP_HEADERS = {
-    //     'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
-    //     'X-RapidAPI-Key': '7d8d051dfdmsh5c0e5a1b489b621p1fb4a2jsn7ab693ce50da'
-    // }
+  const options = {
+    method: 'GET',
+    url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+    params: {
+      q: cityName,
+      units: 'metric'
+    },
+    headers: {
+      'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
+      'X-RapidAPI-Key': '7d8d051dfdmsh5c0e5a1b489b621p1fb4a2jsn7ab693ce50da'
+    }
+  };
 
-    // let response = await axios.get(OPEN_WEATHER_MAP_URL, {q: cityName}, OPEN_WEATHER_MAP_HEADERS);
-    // console.log(response)
-
-    const options = {
-        method: 'GET',
-        url: 'https://community-open-weather-map.p.rapidapi.com/weather',
-        params: {
-          q: cityName,
-          units: 'metric'
-        },
-        headers: {
-          'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
-          'X-RapidAPI-Key': '7d8d051dfdmsh5c0e5a1b489b621p1fb4a2jsn7ab693ce50da'
-        }
-      };
-
-      const response = await axios.request(options)
-    //   console.log(response.data)
-      bot.sendMessage(chatId, `В ${msg.text} сейчас ${Math.round(response.data.main.temp)} градусов`);
-      console.log(msg.text)
-      
-
-  });
+  const response = await axios.request(options)
+  bot.sendMessage(chatId, `В ${msg.text} сейчас ${Math.round(response.data.main.temp)} градусов, ощущается как ${Math.round(response.data.main.feels_like)}. Скорость ветра ${response.data.wind.speed} м/c, давление ${response.data.main.pressure}`);
+  console.log(msg.text)
+});
